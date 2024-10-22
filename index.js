9d0c2c74fb0668ecc791a938038add28647c356d
ShernOC <sherlynea1994@hotmail.com>
2024-10-22 21:48:48 +0300

    minor change

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 // overides the roller-skate satrudayFun("bathe my dog)
 
 function saturdayFun(activity="roller-skate"){
-  return `This Saturday, I want to ${activity}!`;
+  return(`This Saturday, I want to ${activity}!`) ;
 }
-saturdayFun();
-saturdayFun("bathe my dog"); 
+saturdayFun();// function is called 
+saturdayFun("bathe my dog!"); 
 
 //(mondayWork) Defining a function using function expresssion 
 const mondayWork = function(activity="go to the office"){
 return `This Monday, I will ${activity}.`;
 }
-mondayWork(); 
+mondayWork(); // a default argument is created. 
 mondayWork("work from home") // This Monday, I will work from home 
-f
+
 //Define an IIFE : Immediately-Invoked Function Expression 
 function wrapAdjective(fn='*'){
 return function (result = "a hard worker"){
   return `You are ${fn + result + fn}!`;
 };
 }
+
+wrapAdjective();
